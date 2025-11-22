3f50159ba7bc1f69b375fba34b46ae7530899f35
Robbie Fraser <fraserrobbie2@gmail.com>
2025-11-22 07:29:46 +0000

    add login page and add registration folder

--- rota_core/settings.py
+++ rota_core/settings.py
@@ -30,6 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+LOGIN_REDIRECT_URL = "home"          # or "current_month"
+LOGOUT_REDIRECT_URL = "home"
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
