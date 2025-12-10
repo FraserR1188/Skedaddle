d19087447034d02ec1f563d78aea9ed5112170b5
Robbie Fraser <fraserrobbie2@gmail.com>
2025-12-10 15:38:52 +0000

    Add tailwind css to project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./rota/templates/**/*.html",
+    "./**/templates/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
